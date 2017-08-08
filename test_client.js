6838d6a99f3ba6c124067e02867e868a5df5c6ba
Chris Gunawardena <chris@gunawardena.id.au>
2017-08-08 12:57:32 +0000

    mqtt test client update

--- test_client.js
+++ test_client.js
@@ -1,5 +1,5 @@
 var mqtt = require('mqtt')
-var client  = mqtt.connect('mqtt://37.139.12.249')
+var client  = mqtt.connect('mqtt://mqtt.chris.gunawardena.id.au')
  
 client.on('connect', function () {
   client.subscribe('/lights/neopixel');
