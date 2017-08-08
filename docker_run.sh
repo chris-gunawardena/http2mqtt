4a5c230475cca9c8ef26d71ab3ddf7360156c92a
Chris Gunawardena <chris.gun@nl.ibm.com>
2017-08-08 09:47:31 +0200

    docker file fix

--- docker_run.sh
+++ docker_run.sh
@@ -1,1 +1,1 @@
-docker run -p 443:443 -d chrisgunawardena/http2mqtt -v /etc/letsencrypt/live/mqtt.chris.gunawardena.id.au:/etc/letsencrypt/live/mqtt.chris.gunawardena.id.au
+docker run -p 443:443 -d -v /etc/letsencrypt/live/mqtt.chris.gunawardena.id.au:/etc/letsencrypt/live/mqtt.chris.gunawardena.id.au chrisgunawardena/http2mqtt 
