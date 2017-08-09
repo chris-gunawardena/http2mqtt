67e9ab92d5a84a1c2f247508061503e63f5b2306
Chris Gunawardena <chris.gun@nl.ibm.com>
2017-08-09 15:56:07 +0200

    mqtt reply

--- server.js
+++ server.js
@@ -6,6 +6,15 @@ mqtt_server.on('ready', function setup() {
 mqtt_server.on('clientConnected', function(client) {
     console.log('client connected', client.id);
 });
+var responses = {};
+mqtt_server.on('published', function(packet, client) {
+  console.log('Published', packet.topic, packet.payload);
+  if(responses[packet.topic]) {
+      responses[packet.topic].setHeader('Content-type', 'application/json');
+      responses[packet.topic].send(packet.payload);
+      responses[packet.topic] = null;
+  }
+});
 
 var express_app = require('express')();
 var https = require('https');
@@ -15,27 +24,22 @@ var https_options = {
     cert: fs.readFileSync('/etc/letsencrypt/live/' + process.env.SSL_HOST + '/fullchain.pem'),
     ca: fs.readFileSync('/etc/letsencrypt/live/' + process.env.SSL_HOST + '/chain.pem')
 };
+
 express_app.use(require("body-parser").text({type: ()=>true}));
 express_app.all('*', function(req, res){
     console.log(req.path, req.body);
+    // store response until reply from mqtt client
+    let reply_id = '/reply/id_' + Math.ceil(Math.random()*10000);
+    responses[reply_id] = res;
     mqtt_server.publish({
         topic: req.path,
-        payload: req.body,
+        payload: {
+            body: req.body,
+            reply_id: reply_id
+        },
         qos: 0, // 0, 1, or 2
         retain: false // or true
-    }, function() {
-
-        res.setHeader('Content-type', 'application/json');
-        var response = {
-            "speech": "Switching lights on/off",
-            "displayText": "Switching lights on/off",
-            "data": {},
-            "contextOut": [],
-            "source": "mqtt"
-        };
-        res.send(response);
-    })
+    });
 });
-
 https.createServer(https_options, express_app).listen(443);
 
