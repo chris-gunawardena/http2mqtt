37eed6c6d3dc518a13eb71af48c98f5cd969d118
Chris Gunawardena <chris.gun@nl.ibm.com>
2017-08-15 12:36:02 +0200

    added mqtt to https

--- server.js
+++ server.js
@@ -1,5 +1,38 @@
-
+var request = require('request');
+var express_app = require('express')();
+var basicAuth = require('express-basic-auth');
+var https = require('https');
+var fs = require('fs');
 var mqtt_server = new require('mosca').Server({ port: 1883 });
+
+
+var responses = {};
+mqtt_server.on('published', function(packet, client) {
+  console.log('Published', packet.topic, packet.payload);
+  // if there is a response waiting for this topic
+  if (responses[packet.topic]) {
+    responses[packet.topic].setHeader('Content-type', 'application/json');
+    responses[packet.topic].send(packet.payload);
+    responses[packet.topic] = null;
+  } else if(packet.topic.startsWith('/request')) {
+    //make a http request
+    var options = JSON.parse(packet.payload);
+    request(options, (error, response, body) => {
+        if (error) {
+            return console.error('upload failed:', error);
+        }
+        console.log('Upload successful!  Server responded with:', body);
+        if (options.reply_id) {
+            mqtt_server.publish({
+                topic: options.reply_id,
+                payload: JSON.stringify(body),
+                qos: 0, // 0, 1, or 2
+                retain: false // or true
+            });
+        }
+    })
+  }
+});
 mqtt_server.on('ready', function setup() {
   console.log('mqtt_server server is up and running');
 //   mqtt_server.authenticate = function(client, username, password, callback) {
@@ -11,35 +44,15 @@ mqtt_server.on('ready', function setup() {
 mqtt_server.on('clientConnected', function(client) {
     console.log('client connected', client.id);
 });
-var responses = {};
-mqtt_server.on('published', function(packet, client) {
-  console.log('Published', packet.topic, packet.payload);
-  if(responses[packet.topic]) {
-      responses[packet.topic].setHeader('Content-type', 'application/json');
-      responses[packet.topic].send(packet.payload);
-      responses[packet.topic] = null;
-  }
-});
-
-var express_app = require('express')();
-var basicAuth = require('express-basic-auth');
-var https = require('https');
-var fs = require('fs');
-var https_options = {
-    key: fs.readFileSync('/etc/letsencrypt/live/' + process.env.SSL_HOST + '/privkey.pem'),
-    cert: fs.readFileSync('/etc/letsencrypt/live/' + process.env.SSL_HOST + '/fullchain.pem'),
-    ca: fs.readFileSync('/etc/letsencrypt/live/' + process.env.SSL_HOST + '/chain.pem')
-};
 
 // express_app.use(basicAuth({
 //     users: { 'rest_user': 'rest_secret' }
 // }));
 express_app.use(require("body-parser").text({type: ()=>true}));
-
 express_app.all('*', function(req, res){
     console.log(req.path, req.body);
     // store response until reply from mqtt client
-    let reply_id = '/reply/id_' + Math.ceil(Math.random()*10000);
+    var reply_id = '/reply/id_' + Math.ceil(Math.random()*10000);
     responses[reply_id] = res;
     mqtt_server.publish({
         topic: req.path,
@@ -51,5 +64,13 @@ express_app.all('*', function(req, res){
         retain: false // or true
     });
 });
-https.createServer(https_options, express_app).listen(443);
+if (process.env.SSL_HOST) {
+    https.createServer({
+        key: fs.readFileSync('/etc/letsencrypt/live/' + process.env.SSL_HOST + '/privkey.pem'),
+        cert: fs.readFileSync('/etc/letsencrypt/live/' + process.env.SSL_HOST + '/fullchain.pem'),
+        ca: fs.readFileSync('/etc/letsencrypt/live/' + process.env.SSL_HOST + '/chain.pem')
+    }, express_app).listen(443);
+} else {
+    console.log('please setup letsencrypt');
+}
 
